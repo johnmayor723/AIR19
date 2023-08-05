645052e2727da765b4fb66819876cd75e9bd6708
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2023-08-05 01:02:11 -0700

    second commit

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const trackerSchema = new Schema({
+  sname: String,
+  rname: String,
+  saddress: String,
+  paddress: String,
+  pdate: String,
+  ddate: String,
+  clocation: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Tracker", trackerSchema);
