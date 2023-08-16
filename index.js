e0c9a1e7ec454012be8c0ea06505202492193ad2
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2023-08-16 03:42:32 -0700

    added admin logic to index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 const Tracker = require("./models")
 const mongoose = require("mongoose")
 const path = require('path')
-
+const  methodOverride = require("method-override");
 const connectEnsureLogin = require('connect-ensure-login');
 const passport = require('passport');
 const LocalStrategy = require('passport-local')
@@ -26,6 +26,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded())
 // Set up session
+app.use(methodOverride('_method'));
 app.use(
   session({
     secret: "mooohdhfhgfgfggggbb55544@@!@#$$FTtvsvv4435ffv",
@@ -110,14 +111,39 @@ app.get("/campgrounds/:id", function(req, res){
   })
 
 })
+app.put('/campgrounds/:id', async (req, res) => {
+  const {id} = req.params;
+  const rid ={_id:id}
+  let data = req.body
+  
+  console.log(data)
+  await Tracker.findByIdAndUpdate(id, {...data})
+  .then(newdata=>{
+    res.redirect('/admin')
+    console.log(newdata)
+  })
+  
+ 
+  
+  /*await Tracker.create(data)
+  .then(newdata=>{
+    res.redirect('/admin')
+    console.log(newdata)
+  })*/
+  
+})
+app.delete('/campgrounds/:id', async (req, res) => {
+  await Tracker.findOneAndDelete(req.params.id)
 
+  res.redirect('/admin')
+})
 app.post('/create', function(req, res){
   let data = req.body
   
   const {sname, saddress, rname, paddress, pdate, ddate, clocation, tnumber}= data
   Tracker.create(data)
   .then(data=>{
-    res.send(data)
+    res.redirect("/admin")
   })
   .catch(err=>{
     console.log(err)
@@ -172,10 +198,9 @@ app.get("/login", function(req, res){
 //handling login logic
 app.post("/login", passport.authenticate("local", 
    {
-       successRedirect: "/campgrounds",
+       successRedirect: "/",
        failureRedirect: "/login",
-       failureFlash: true,
-       successFlash: 'Welcome to YelpCamp!'
+       
    }), function(req, res){
 });
 
